59e1b01723f2d92f7a272f517e44a7527c36901f
Anton Kaliaev <anton.kalyaev@gmail.com>
2021-12-18 13:10:03 +0400

    add index_all_columns and index_column functions
    anyhow crate is added for reporting errors

--- src/rover.rs
+++ src/rover.rs
@@ -1,8 +1,11 @@
+use anyhow::Result;
+
 use std::collections::{BTreeMap, HashMap};
 
-// There can't be more than 255 columns.
+/// Column's index (0...255)
 pub type ColumnIndex = u8;
 
+/// ValueDecoder transforms a value into a vector of Columns.
 pub trait ValueDecoder<V>
 where
     V: AsRef<[u8]>,
@@ -10,12 +13,15 @@ where
     fn decode(&self, value: V) -> Vec<Column>;
 }
 
-// Column can either be a i32 or a String.
+/// Column can either be a i32 or a String.
 pub enum Column {
     Number(i32),
     Str(String),
 }
 
+/// Rover is an inmemory indexer, which can be used to index any KV database. A `value_decoder` is
+/// used to transform a value into a vector of Columns. Then, for each column, a HashMap and
+/// BTreeMap are built. A hashmap gives O(1) access, a btree map gives us sorted list.
 pub struct Rover<K, V>
 where
     K: AsRef<[u8]>,
@@ -41,22 +47,43 @@ where
             value_decoder,
         }
     }
+
+    pub fn index_all_columns(&self, k: K, v: V) -> Result<()> {
+        let columns = self.value_decoder.decode(v);
+        for c in &columns {
+            self.index_column(k.as_ref(), c)?;
+        }
+        Ok(())
+    }
+
+    fn index_column(&self, k: &[u8], c: &Column) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::decoder::borsh::BorshValueDecoder;
+
+    struct SingleStringValueDecoder {}
+    impl<V> ValueDecoder<V> for SingleStringValueDecoder
+    where
+        V: AsRef<[u8]>,
+    {
+        fn decode(&self, v: V) -> Vec<Column> {
+            let mut columns = Vec::new();
+            let s = String::from_utf8(v.as_ref().to_vec()).unwrap();
+            columns.push(Column::Str(s));
+            columns
+        }
+    }
 
     #[test]
-    fn it_indexes_all_keys() {
-        use std::collections::BTreeMap;
-
-        let mut map = BTreeMap::new();
-        map.insert(3, "c");
-        map.insert(2, "b");
-        map.insert(1, "a");
-
-        // let value_decoder = gg
-        let mut r = Rover::new(value_decoder);
+    fn it_indexes_all_columns() {
+        let r: Rover<&[u8], &[u8]> = Rover::new(Box::new(BorshValueDecoder {}));
+        for (k, v) in [(1, "a"), (2, "b"), (3, "c")] {
+            r.index_all_columns(&(k as i32).to_be_bytes(), v.as_bytes());
+        }
     }
 }
